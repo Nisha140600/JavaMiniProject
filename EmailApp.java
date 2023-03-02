1363a7afdbcc5afbc74d5876e06aeb899f7c6737
Nisha140600 <126701391+Nisha140600@users.noreply.github.com>
2023-03-02 09:46:45 +0530

    Email.java EmailApp.java package-info.java
    Email.java
    EmailApp.java
    package-info.java

---
+++ EmailApp.java
@@ -0,0 +1,10 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Nisha Pardeshi","Mithila Dhumal");
+		System.out.println(em1.showInfo());
+	}
+
+}
